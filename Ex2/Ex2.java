81bf87088d58ce59e71f8bc8226f8314e5f2f34d
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2020-01-03 22:23:19 +0700

    Ex2

---
+++ Ex2/Ex2.java
@@ -0,0 +1,17 @@
+class Ex2 {
+    public static void main(String[] args){
+        char initial = 'S';
+        String name = "SunvoDz";
+        int age = 24;
+        float money_a = 0.1f;
+        double money_b = 1234567;
+        boolean gender_man = true; 
+        System.out.println("Initial : "+initial);
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+        System.out.println("Gender Man : "+gender_man);
+        System.out.println("Money ABank : "+money_a);
+        System.out.println("Money BBank : "+money_b);
+
+    }
+}
