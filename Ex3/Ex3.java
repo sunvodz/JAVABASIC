21914511fccb7201c140d43c0db9a906888e78e2
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2020-01-03 22:44:48 +0700

    Ex3

---
+++ Ex3/Ex3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Ex3{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter :");
+        String in = input.nextLine(); // String
+        //int in = input.nextInt(); // int
+        //float in = input.nextFloat(); // float
+        System.out.println("Result :"+in);
+    }
+}
