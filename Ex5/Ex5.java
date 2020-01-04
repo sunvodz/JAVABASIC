0836ed3d02a6d4ffec3689fe4002b3148ef99610
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2020-01-04 07:10:43 +0700

    Ex5

---
+++ Ex5/Ex5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Ex5{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter A : ");
+        int num_a = input.nextInt();
+        
+
+        System.out.print("Enter B : ");
+        int num_b = input.nextInt();
+        System.out.println(num_a + " And " + num_b);
+
+        // == , != , < , > , <= , >=
+        //&& (T&&T = T), || (F||F=F), ! (T = F , F = T) 
+
+        boolean result = (num_a != num_b) && (num_a < num_b);
+        
+        System.out.println("Result : " +result);
+
+    }
+}
