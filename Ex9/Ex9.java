dfe5d4191cf159f5ebbac70227634f17bceb47d6
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2020-01-04 17:58:44 +0700

    Ex9

---
+++ Ex9/Ex9.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Ex9{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number : ");
+        int number = input.nextInt();
+
+        // oddเลขคี่ and evenเลขคู่ 
+        // if(number % 2 == 0){
+        //     System.out.print("Number is Even");
+        // }else{
+        //     System.out.print("Number is Odd");
+        // }
+
+        //number 1256 => 125
+        // int num = number / 10;
+        // System.out.print("Number : " + num);
+
+        //number 1256 => 6
+        // int num = number % 10;
+        // System.out.print("Number : " + num);
+
+
+    }
+}
